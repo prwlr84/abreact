7a99f78cc2e3a4d55609816ebd8a9fe7d5a6a474
Antal Bako <prowlersk8@gmail.com>
2021-03-15 14:39:43 +0100

    location based greeting added

--- src/containers/main.jsx
+++ src/containers/main.jsx
@@ -3,35 +3,39 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Typed from 'typed.js';
+import { setLang } from '../actions';
+import Menu from './menu';
 
 class Main extends Component {
   componentDidMount(){
-    let typed = new Typed('#typed', {
-      strings: [
-      "<p>Hello! Welcome!</p>^1000\n<p>I'm Antal Bako</p>^1000\n<p>Full Stack Web Developer</p>^1000\n<p>Please, choose from the following options:</p>^1000\n<p>1 | About</p>^1000\n<p>2 | Work</p>^1000\n<p>3 | Connect</p>"],
-      typeSpeed: 20,
-      loop: false,
-      showCursor: false
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.refresher);
+    this.props.setLang(this.props.ip);
   }
 
   render() {
     return (
       <div className="app row">
         <div className="main col-12 col-sm-6">
-          <h1 id="typed"></h1>
-          <form action="">
-            <input type="text"/>
-            <button type="submit">GO</button>
-          </form>
+          { this.props.lang ? <Menu /> : <h1>Loading...</h1> }
         </div>
       </div>
     );
   }
 }
 
-export default Main;
+function mapDispatchToProps(dispatch) {
+ return bindActionCreators(
+ { setLang: setLang },
+ dispatch
+ );
+}
+
+function mapStateToProps(state) {
+ return {
+ ip: state.ip,
+ lang: state.lang
+ };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps) (Main);
+
--- src/index.jsx
+++ src/index.jsx
@@ -5,19 +5,29 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
+import { logger } from 'redux-logger';
 
 import '../assets/stylesheets/application.scss';
-import Main from './containers/main.jsx'
+import Main from './containers/main.jsx';
+import langReducer from './reducers/lang_reducer.js';
 
+const initialState = {
+  //ip: JSON.parse(app.dataset.ip),
+  //ip: '5.38.128.0',
+  //ip: '170.253.46.238',
+  ip: '1.4.255.255',
+  lang: ""
+}
 const reducers = combineReducers({
-  curusr: (state = null, action) => state
+  ip: (state = null, action) => state,
+  lang: langReducer
 });
 
-const middlewares = applyMiddleware(reduxPromise);
+const middlewares = applyMiddleware(logger, reduxPromise);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={createStore(reducers, initialState, middlewares)}>
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={Main} />
