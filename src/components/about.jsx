993549e0e32592f4c3bde8f4b687791f966bb283
Antal Bako <prowlersk8@gmail.com>
2021-03-17 21:43:48 +0100

    layout fixed, img glitch on the way

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,24 +8,49 @@ class About extends Component {
         <div className="about row">
             <div className="about1">
               <div className="about-text1">
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Excepturi, debitis.</h3>
+                <h2>Hi there!</h2>
+                <h3>My name is Antal Bako</h3>
+                <h4>I have been communicating with humans for living for more than 10 years, now let's try with computers!<br/>
+                  After more than a decade in sales, felt time for a change.<br/>
+                  I have decided to return my old passion and successfully completed Le Wagon's last Web Development bootcamp in Amsterdam. I had great fun with all the challenges, but the camp is over and I'm always up for new ones!<br/>
+                  Looking for new opportunities in Web Development, with experience in React JS, Ruby on Rails, AJAX, relational and non-relational databases and more. Absolutely open for new languages with excellent capacity to retain new things.
+                </h4>
               </div>
               <div className="frame3"><img src="https://res.cloudinary.com/prwlr84/image/upload/v1615874393/resized-image-Promo_fg0i43.jpg" alt="Photo in a suit" /></div>
               <div className="frame3"><img src="https://res.cloudinary.com/prwlr84/image/upload/v1607610545/f67fwf2e07stxitmpmfv0gfu3ui8.jpg" alt="Sunny casual photo" /></div>
             </div>
             <div className="about2">
               <div className="frame3"><img src="https://res.cloudinary.com/prwlr84/image/upload/v1615874393/IMG_20190908_224113_zrth35.jpg" alt="Budapest" /></div>
-              <div className="about-text2">
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Excepturi, debitis.</h3>
-              </div>
               <div className="frame3"><img src="https://res.cloudinary.com/prwlr84/image/upload/v1615874393/IMG_20181010_085053_owroqb.jpg" alt="Amsterdam" /></div>
+              <div className="about-text2">
+                <h4>I was born in a town in the middle of the Hungarian lowlands, then I moved to Budapest for my studies and I lived there for 10 years.<br/> Next stop was the evenly
+                flat Amsterdam and currently moved to Spain to try a bit more "elevated" lifestyle in the mountains.</h4>
+              </div>
             </div>
             <div className="about3">
               <div className="about-text3">
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Excepturi, debitis.</h3>
+                <h4>In my free time I love to pedal, flat like Holland or hills like in Andalucia, doesn't matter.<br/>
+                  Love movies a lot, Netflix is gettig boring...<br/>
+                  And music! Everything from classics through jazz to electronic music, but no mainstream please!</h4>
               </div>
-              <div className="frame3"><img src="https://res.cloudinary.com/prwlr84/image/upload/v1615874393/resized-image-Promo_1_s9hky3.jpg" alt="Holland" /></div>
-              <div className="frame3"><img src="https://res.cloudinary.com/prwlr84/image/upload/v1615874393/IMG_20210102_153256_wfwk3u.jpg" alt="Mountain" /></div>
+              <div className="frame3">
+                <div class="glitchH">
+                  <div class="glitch-img1"></div>
+                  <div class="glitch-img1"></div>
+                  <div class="glitch-img1"></div>
+                  <div class="glitch-img1"></div>
+                  <div class="glitch-img1"></div>
+                </div>
+              </div>
+              <div className="frame3">
+                <div class="glitchL">
+                  <div class="glitch-img2"></div>
+                  <div class="glitch-img2"></div>
+                  <div class="glitch-img2"></div>
+                  <div class="glitch-img2"></div>
+                  <div class="glitch-img2"></div>
+                </div>
+              </div>
               <div className="frame3"><video autoPlay loop src="https://res.cloudinary.com/prwlr84/video/upload/v1615825455/vlc-record-2021-03-15-17h07m54s-GOPR0685.MP4-_convert-video-online.com_1_kd3qwx.mp4" alt="Mountain bike ride" /></div>
             </div>
         </div>
@@ -38,3 +63,4 @@ class About extends Component {
 }
 
 export default About;
+
