2d956fb1286b25521f5d957d159fd2613fae5e82
Antal Bako <prowlersk8@gmail.com>
2021-04-12 16:34:37 +0200

    more list, seed

---
+++ src/components/fakerank.js
@@ -0,0 +1,29 @@
+ const fakerank = [
+  {
+    "point": 1447,
+    "name": "Elva",
+    "address": "Belgium"
+  },
+  {
+    "point": 2637,
+    "name": "Erickson",
+    "address": "Mexico"
+  },
+  {
+    "point": 2772,
+    "name": "Winters",
+    "address": "Aruba"
+  },
+  {
+    "point": 3027,
+    "name": "Althea",
+    "address": "Poland"
+  },
+  {
+    "point": 3403,
+    "name": "Alexandria",
+    "address": "Guyana"
+  }
+];
+
+export default fakerank;
--- src/components/ranks.jsx
+++ src/components/ranks.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import fakerank from './fakerank';
 
 class Ranks extends Component {
   render(){
     return(
-      <div className="ranks">
-        <h3>dsfgsdfg</h3>
+      <div className="ranks col-sm-6" style={{display: 'none'}}>
+        <h3>The best scores:</h3>
         <ul>
-          <li>
-            <p>asdfas</p>
-            <p>zxcvzx</p>
-            <p>zvxcv</p>
+        {fakerank.sort().map(data => {return(
+          <li key='data.name'>
+            <p>{data.name}</p>
+            <p>{data.point}</p>
+            <p>{data.address}</p>
           </li>
+        )})}
         </ul>
       </div>
     )
